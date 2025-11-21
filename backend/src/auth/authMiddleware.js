8d1875a85338ed2d521ddb113df9f43b51b9e10c
Julietwanza <140821704+Julietwanza@users.noreply.github.com>
2025-11-21 08:39:55 +0300

    Create authMiddleware.js

---
+++ backend/src/auth/authMiddleware.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const auth = (req, res, next) => {
+  const token = req.header("x-auth-token"); // Standard header for JWT
+
+  if (!token) {
+    return res.status(401).json({ message: "No token, authorization denied." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user; // Add user payload from token to request
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Token is not valid." });
+  }
+};
+
+export default auth;
